ecdb157e73cd636d26a435342f77dcf0ddb94aa2
Victor_2103 <vitcheffvi@cy-tech.fr>
2023-02-08 19:12:15 +0100

    [curl] try to connect to ifpi

--- curl.py
+++ curl.py
@@ -1,2 +1,15 @@
 import requests
+import os
+from dotenv import load_dotenv
 
+load_dotenv(".env")
+
+
+body={"username":str(os.getenv("NAME")),"password":str(os.getenv("PASSWORD"))}
+
+print(str(os.getenv("NAME")))
+print(str(os.getenv("PASSWORD")))
+
+x=requests.post("https://registre-national-entreprises-pprod.inpi.fr/api/sso/login",json=body)
+
+print(x.text)
