174f3f6fb9b45ea09754b04b35939f8cb8137072
Victor_2103 <vitcheffvi@cy-tech.fr>
2023-02-08 19:35:09 +0100

    [curl] try to read file in my_data

--- curl.py
+++ curl.py
@@ -1,7 +1,7 @@
 import requests
 import os
 from dotenv import load_dotenv
-
+import pandas as pd
 load_dotenv(".env")
 
 
@@ -15,3 +15,7 @@ x = requests.post(
     "https://registre-national-entreprises-pprod.inpi.fr/api/sso/login", json=body)
 
 print(x.text)
+
+data=pd.read_csv("my_data/if155.xls")
+
+print(data)
