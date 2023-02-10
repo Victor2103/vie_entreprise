1813b805b251d44137368e30d338b1ee166c71ce
Victor_2103 <vitcheffvi@cy-tech.fr>
2023-02-10 12:20:08 +0100

    [curl] get brands with radiations

--- curl.py
+++ curl.py
@@ -24,10 +24,13 @@ print(data)
 y = requests.get("https://bodacc-datadila.opendatasoft.com/api/records/1.0/search/?dataset=annonces-commerciales&q=Siren&facet=publicationavis&facet=publicationavis_facette&facet=typeavis&facet=typeavis_lib&facet=familleavis&facet=familleavis_lib&facet=numerodepartement&facet=departement_nom_officiel&refine.familleavis_lib=Ventes+et+cessions")
 # print(y.text)
 
-# Transform into a json file
-data = y.json()
 
-print(data)
+z = requests.get('https://bodacc-datadila.opendatasoft.com/api/records/1.0/search/?dataset=annonces-commerciales&q=&rows=100&sort=dateparution&facet=publicationavis&facet=publicationavis_facette&facet=typeavis&facet=typeavis_lib&facet=familleavis&facet=familleavis_lib&facet=numerodepartement&facet=departement_nom_officiel&refine.departement_nom_officiel=Alpes-Maritimes&refine.familleavis_lib=Radiations&refine.typeavis_lib=Avis+d%E2%80%99annulation')
+
+# Transform into a json file
+data = z.json()
+
+print(data["records"][1]["fields"])
 
 with open("test.json", "w") as f:
     json.dump(data, f)
