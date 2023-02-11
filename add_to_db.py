014aeab0df59abcb9dd5b6703856123d51ef8dcd
Victor_2103 <vitcheffvi@cy-tech.fr>
2023-02-11 11:41:49 +0100

    [add_to_db.py] print the siren in console

--- add_to_db.py
+++ add_to_db.py
@@ -1,11 +1,14 @@
 import json
 
-with open("test.json","r") as f:
-    data=json.load(f)
+with open("my_data/insee.json", "r") as f:
+    data = json.load(f)
 
-
+"""
 # get the siren
 tmp=json.loads(data["records"][1]["fields"]["listepersonnes"])
 siren=tmp['personne']['numeroImmatriculation']['numeroIdentification']
 
 print(siren)
+"""
+
+print(data["unitesLegales"][0]['siren'])
