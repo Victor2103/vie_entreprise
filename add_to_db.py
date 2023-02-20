5d64ee1bcb4e31d2ebf91da63c5b7f2f522b758e
Victor <vitcheffvi@cy-tech.fr>
2023-02-20 12:02:17 +0100

    [add_to_db.py] print var to add in db

--- add_to_db.py
+++ add_to_db.py
@@ -1,16 +1,15 @@
 import json
 
-with open("my_data/insee_9.json", "r") as f:
-    data = json.load(f)
-
-"""
-# get the siren
-tmp=json.loads(data["records"][1]["fields"]["listepersonnes"])
-siren=tmp['personne']['numeroImmatriculation']['numeroIdentification']
-
-print(siren)
-"""
-for i in data["unitesLegales"]:
-    if i["categorieEntreprise"]:
-        print(i["siren"])
+cpt = 0
+for i in range(0, 40):
+    with open(f"my_data/insee_{i}.json", "r") as f:
+        data = json.load(f)
+    for j in data["unitesLegales"]:
+        cpt += 1
+        print(j["siren"])
+        print(j["nombrePeriodesUniteLegale"])
+        print(j["dateCreationUniteLegale"])
+        if j["categorieEntreprise"]:
+            print(j["categorieEntreprise"])
+print(cpt)
 # print(data["unitesLegales"][0]['siren'])
