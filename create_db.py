d63164011409d7c9ebe718e6af83c900d7aa35ba
Victor <vitcheffvi@cy-tech.fr>
2023-02-20 19:01:11 +0100

    [create_db] script for creation of the database

--- create_db.py
+++ create_db.py
@@ -16,15 +16,17 @@ cursor = connection.cursor()
 # The command to create the database vie_entreprise
 command = 'CREATE DATABASE vie_entreprise'
 
+
 # Set the auto commit to avoid an error
 auto_comit = psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT
 connection.set_isolation_level(auto_comit)
 
 
-# Create the database
+# Create the database and add extension
 cursor.execute(command)
 
-
+# Command to save in the cluster the change
+connection.commit()
 # Close the database
 cursor.close()
 connection.close()
