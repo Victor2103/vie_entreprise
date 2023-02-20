93ec942ac9d75c8a0d3c1be5b24cc86138e7c8bd
Victor <vitcheffvi@cy-tech.fr>
2023-02-20 15:17:07 +0100

    [create_db] script to create the database

---
+++ create_db.py
@@ -0,0 +1,30 @@
+# Import the library for the database.
+import psycopg2
+
+import os
+
+from dotenv import load_dotenv
+load_dotenv(".env")
+
+# Make a connection to the postgresql database
+# Connect to the database
+connection = psycopg2.connect(
+    f"postgres://{str(os.getenv('USERNAME'))}:{str(os.getenv('PASSWORD'))}@postgresql-2791bab0-od486479f.database.cloud.ovh.net:20184/electric?sslmode=require")
+cursor = connection.cursor()
+
+
+# The command to create the database vie_entreprise
+command = 'CREATE DATABASE vie_entreprise'
+
+# Set the auto commit to avoid an error
+auto_comit = psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT
+connection.set_isolation_level(auto_comit)
+
+
+# Create the database
+cursor.execute(command)
+
+
+# Close the database
+cursor.close()
+connection.close()
