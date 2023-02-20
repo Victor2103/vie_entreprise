ae6c4b8197fdc641fab6a4ca84fe0cc40b31a6cf
Victor <vitcheffvi@cy-tech.fr>
2023-02-20 12:02:40 +0100

    [insee.py] get all brand from 1980 to 2019

--- insee.py
+++ insee.py
@@ -15,3 +15,27 @@ for i in range(10):
     # print(data.json())
     with open(f"my_data/insee_{i}.json", "w") as f:
         json.dump(data.json(), f)
+
+for i in range(10):
+    url = f'https://api.insee.fr/entreprises/sirene/V3/siren?q=dateCreationUniteLegale:[199{i} TO 199{i+1}]'
+    data = requests.get(url=url, headers=headers)
+    print(data)
+    # print(data.json())
+    with open(f"my_data/insee_{i+10}.json", "w") as f:
+        json.dump(data.json(), f)
+
+for i in range(10):
+    url = f'https://api.insee.fr/entreprises/sirene/V3/siren?q=dateCreationUniteLegale:[200{i} TO 200{i+1}]'
+    data = requests.get(url=url, headers=headers)
+    print(data)
+    # print(data.json())
+    with open(f"my_data/insee_{i+20}.json", "w") as f:
+        json.dump(data.json(), f)
+
+for i in range(10):
+    url = f'https://api.insee.fr/entreprises/sirene/V3/siren?q=dateCreationUniteLegale:[201{i} TO 201{i+1}]'
+    data = requests.get(url=url, headers=headers)
+    print(data)
+    # print(data.json())
+    with open(f"my_data/insee_{i+30}.json", "w") as f:
+        json.dump(data.json(), f)
