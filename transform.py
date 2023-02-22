db6c959ad8b22626ba1ffcb5b4e049b74b490cad
Victor <vitcheffvi@cy-tech.fr>
2023-02-22 17:10:04 +0100

    [transform] take start and end date

--- transform.py
+++ transform.py
@@ -1,4 +1,3 @@
-import json
 # Import the library for the database.
 import psycopg2
 
@@ -12,6 +11,19 @@ load_dotenv(".env")
 connection = psycopg2.connect(
     f"postgres://{str(os.getenv('USERNAME'))}:{str(os.getenv('PASSWORD'))}@postgresql-2791bab0-od486479f.database.cloud.ovh.net:20184/vie_entreprise?sslmode=require")
 cursor = connection.cursor()
+
+cursor.execute("SELECT siren FROM brand WHERE number_legal_units > 1 ;")
+
+
+# print(cursor.fetchall())
+sirens = cursor.fetchall()
+cursor.close()
+cursor = connection.cursor()
+# print(sirens[0][0])
+for i in range(len(sirens)):
+    cursor.execute(
+        "SELECT start_date,end_date,siren FROM legal_units WHERE siren=%s ;", [sirens[i][0]])
+    # print(cursor.fetchall())
 
 
 # Close the database
