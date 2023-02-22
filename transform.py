777b8365ae97e7710c78f20591017f409de5b808
Victor <vitcheffvi@cy-tech.fr>
2023-02-22 15:47:45 +0100

    [transform] file  for difference start end date

---
+++ transform.py
@@ -0,0 +1,19 @@
+import json
+# Import the library for the database.
+import psycopg2
+
+import os
+
+from dotenv import load_dotenv
+load_dotenv(".env")
+
+# Make a connection to the postgresql database
+# Connect to the database
+connection = psycopg2.connect(
+    f"postgres://{str(os.getenv('USERNAME'))}:{str(os.getenv('PASSWORD'))}@postgresql-2791bab0-od486479f.database.cloud.ovh.net:20184/vie_entreprise?sslmode=require")
+cursor = connection.cursor()
+
+
+# Close the database
+cursor.close()
+connection.close()
